a2564b80c7dc4e3d8e68176e00119a8b43b7e216
hsyoo <hsyoodev@icloud.com>
2026-09-15 00:23:37 +0900

    feat: 엔드포인트 명세

--- src/main/kotlin/io/github/demo/controller/UserRestController.kt
+++ src/main/kotlin/io/github/demo/controller/UserRestController.kt
@@ -1,9 +1,14 @@
 package io.github.demo.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,8 +17,26 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1")
 class UserRestController {
 
-    @GetMapping("/users")
-    fun getUsers(): ResponseEntity<Map<String, Int>> {
+    @Operation(
+        summary = "전체 유저 Read",
+        description = "전체 유저 조회",
+        parameters = [
+            Parameter(
+                name = " test",
+                description = "test",
+                required = true,
+                `in` = ParameterIn.PATH
+            )
+        ],
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "성공"
+            )
+        ]
+    )
+    @GetMapping("/users/{test}")
+    fun getUsers(@PathVariable("test") test: Int): ResponseEntity<Map<String, Int>> {
         val map = mapOf(Pair("James", 13))
 
         return ResponseEntity
